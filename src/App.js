cb7e25d4085a5a360f363c8d74e6145604f86491
Muluken Demas <mulerian@gmail.com>
2024-04-24 10:27:19 +0300

    ref app demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React,{useRef} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [query, setQuery] = React.useState("react hooks");
+    /* We can pass useRef a default value.
+     We don't need it here, so we pass in null to reference an empty object
+  */
+    const searchInput = useRef(null);
+
+    function handleClearSearch() {
+      /* 
+      .current references the input element upon mount
+      useRef can store basically any value in its .current property
+    */
+      searchInput.current.value = "";
+      searchInput.current.focus();
+    }
+
+    return (
+      <form style={{padding:'20px'}}>
+        <input
+          type="text"
+          onChange={(event) => setQuery(event.target.value)}
+          ref={searchInput}
+        />
+        <button type="submit">Search</button>
+        <button type="button" onClick={handleClearSearch}>
+          Clear
+        </button>
+      </form>
+    );
+
 }
 
 export default App;
